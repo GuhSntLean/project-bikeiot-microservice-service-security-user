ddc5c29e885a2bcc0fca74c7187823c1e0cbfaa8
GuhSntLean <guleandro.66@gmail.com>
2022-08-16 01:09:47 -0300

    add librarys

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
+
+app.use(json);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
