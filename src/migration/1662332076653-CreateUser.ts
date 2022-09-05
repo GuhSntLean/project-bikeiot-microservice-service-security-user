e465703ad814daac98287d73c16b0c8314105837
GuhSntLean <guleandro.66@gmail.com>
2022-09-04 22:00:43 -0300

    create migrations and refactory models

--- src/migration/1662332076653-CreateUser.ts
+++ src/migration/1662332076653-CreateUser.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class createUser1662332076653 implements MigrationInterface {
+export class CreateUser1662332076653 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
