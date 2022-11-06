5f8529860004a2c9896a7482633e1d0bb4643ff3
GuhSntLean <guleandro.66@gmail.com>
2022-11-06 00:19:53 -0300

    adding basic refresh token

--- src/controller/RefreshTokenController.ts
+++ src/controller/RefreshTokenController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import { RefreshTokenUseCase } from "../usecase/RefreshTokenUseCase";
 
 class RefreshTokenController {
   async refreshToken(request: Request, response: Response) {
+    const refresh_token = request.body;
 
+    if (refresh_token) {
+      return new Error("Not authorized");
+    }
+
+    const refreshToken = new RefreshTokenUseCase();
+    const token = refreshToken.verifyToken(refresh_token);
+
+    return response.json(token);
   }
 }
 
--- src/usecase/RefreshTokenUseCase.ts
+++ src/usecase/RefreshTokenUseCase.ts
@@ -1,15 +1,31 @@
+import { TokenProvider } from "../provider/TokenProvider";
 import { refreshTokenRepository } from "../repository/RefreshTokenRepository";
 
 class RefreshTokenUseCase {
   async generateRefreshToken(userId: string) {
-    const existRefreshToken = refreshTokenRepository.find({where: { user: {id: userId} }});
-    
-    if(existRefreshToken){
-      refreshTokenRepository.delete({user:{id: userId}});
+    const existRefreshToken = refreshTokenRepository.find({
+      where: { user: { id: userId } },
+    });
+
+    if (existRefreshToken) {
+      refreshTokenRepository.delete({ user: { id: userId } });
     }
   }
 
-  async verifyToken() {}
+  async verifyToken(refreshTokenId: string) {
+    const existRefreshToken = await refreshTokenRepository.findOneBy({
+      id: refreshTokenId,
+    });
+
+    if (!existRefreshToken) {
+      return new Error("Not authorized");
+    }
+
+    const tokenProvider = new TokenProvider();
+    const token = tokenProvider.execute(existRefreshToken.user.id);
+
+    return token;
+  }
 }
 
 export { RefreshTokenUseCase };
