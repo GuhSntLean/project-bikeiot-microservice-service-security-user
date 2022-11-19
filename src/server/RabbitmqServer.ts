affd6eaada7c5fa926d5794fbe9ebdd9e3b930cf
GuhSntLean <guleandro.66@gmail.com>
2022-11-19 11:12:46 -0300

    configure uri to producer service

--- src/server/RabbitmqServer.ts
+++ src/server/RabbitmqServer.ts
@@ -1,8 +1,15 @@
-import { Channel, Connection } from "amqplib";
+import { Channel, connect, Connection } from "amqplib";
 
 class RabbitmqServer {
   private conn: Connection;
   private channel: Channel;
+
+  constructor(private uri: string) {}
+
+  async start() {
+    this.conn = await connect(this.uri);
+    this.channel = await this.conn.createChannel();
+  }
 
   async publishExchange(exchange: string, routingKey: string, message: string) {
     return this.channel.publish(exchange, routingKey, Buffer.from(message));
