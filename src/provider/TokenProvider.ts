dfb5a0e3cde030d18761ea59c12235dfeb9b9d4e
GuhSntLean <guleandro.66@gmail.com>
2023-09-13 18:14:12 -0300

    modify time expire token in code

--- src/provider/TokenProvider.ts
+++ src/provider/TokenProvider.ts
@@ -8,7 +8,7 @@ class TokenProvider {
 
     const token = sign({}, "79123427-290f-4c63-aca3-120ea5364159", {
       subject: userId,
-      expiresIn: "100s",
+      expiresIn: "1h",
     });
 
     return token;
